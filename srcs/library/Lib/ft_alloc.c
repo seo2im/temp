d55587e25cfff2297f7dcd001576427748c9b336
seo1im <seolim6492@gmail.com>
2021-01-13 16:59:43 +0900

    update

--- srcs/library/Lib/ft_alloc.c
+++ srcs/library/Lib/ft_alloc.c
@@ -6,7 +6,7 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 13:19:59 by seolim            #+#    #+#             */
-/*   Updated: 2021/01/13 13:30:33 by seolim           ###   ########.fr       */
+/*   Updated: 2021/01/13 16:59:29 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ char	*single_alloc(int len)
 
 	if (!(alloc = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
+	i = -1;
 	while (++i < len + 1)
 		alloc[i] = '\0';
 	return (alloc);
